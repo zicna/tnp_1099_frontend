65c3363de4bf2a8703109dfc6fe6af98274e4ace
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2023-03-12 23:03:07 -0500

    add ReactRouter 6, wrapp App.js so component of router can be used inside

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import RoutesContainer from './containers/RoutesContainer';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello from TNP_1099</h1>
+      <RoutesContainer></RoutesContainer>
+
     </div>
   );
 }
