1db1a91a0e4fee67c4b6fb6e603213df13074fb1
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2023-03-12 23:13:36 -0500

    add COmponents folder with NavBar component

---
+++ src/components/NavBar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+export default function NavBar() {
+  return (
+    <div>
+        <li>
+            <NavLink to="/home">HOME</NavLink>
+        </li>
+        <li>
+            <NavLink to="/about">ABOUT</NavLink>
+        </li>
+        <li>
+            <NavLink to="/contact">CONTACT</NavLink>
+        </li>
+        <li>
+            <NavLink to="/profile">PROFILE</NavLink>
+        </li>
+    </div>
+  )
+}
