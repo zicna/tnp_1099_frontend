80d1c2f7552369acbc590497da80649409cb64b3
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2023-03-12 23:02:19 -0500

    add containers folder with ROutesContainer file

---
+++ src/containers/RoutesContainer.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import About from "../pages/About";
+import Home from "../pages/Home"
+import Profile from "../pages/Profile"
+import Welcome from "../pages/Welcome"
+import Contact from "../pages/Contact"
+
+export default function RoutesContainer(){
+    return(
+        <div>
+            <Routes>
+                <Route path="/" element={<Welcome/>}></Route>
+                <Route path="/home" element={<Home />}></Route>
+                {/* <Route path="/profile" children={()=><Profile></Profile>}></Route> */}
+                <Route path="/profile" element={<Profile/>}></Route>
+                <Route path="/about" element={<About/>}></Route>
+                <Route path="/contact" element={<Contact/>}></Route>
+
+            </Routes>
+        </div>
+
+    )
+}
