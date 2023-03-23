9e2b63918187217006e15c739a23d4039a875b50
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2023-03-22 19:14:54 -0500

    conditional render link and route to '/profile' based off presence of user in context

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,24 +1,30 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { NavLink } from 'react-router-dom'
+import { UserContext } from '../storeAndContext/UserProvider'
 
 export default function NavBar() {
+  const { user } = useContext(UserContext)
   return (
     <div>
+      <li>
+        <NavLink to="/home">HOME</NavLink>
+      </li>
+      {user ? (
         <li>
-            <NavLink to="/home">HOME</NavLink>
+          <NavLink to="/profile">PROFILE</NavLink>
         </li>
-        <li>
-            <NavLink to="/profile">PROFILE</NavLink>
-        </li>
-        <li>
-            <NavLink to="/login">LOGIN</NavLink>
-        </li>
-        <li>
-            <NavLink to="/about">ABOUT</NavLink>
-        </li>
-        <li>
-            <NavLink to="/contact">CONTACT</NavLink>
-        </li>
+      ) : (
+        ''
+      )}
+      <li>
+        <NavLink to="/login">LOGIN</NavLink>
+      </li>
+      <li>
+        <NavLink to="/about">ABOUT</NavLink>
+      </li>
+      <li>
+        <NavLink to="/contact">CONTACT</NavLink>
+      </li>
     </div>
   )
 }
--- src/containers/RoutesContainer.js
+++ src/containers/RoutesContainer.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Route, Routes } from "react-router-dom";
 import About from "../pages/About";
 import Home from "../pages/Home"
@@ -7,15 +7,18 @@ import Welcome from "../pages/Welcome"
 import Contact from "../pages/Contact"
 import PageNotFound from "../pages/PageNotFound";
 import Auth from "../pages/Auth/Auth";
+import { UserContext } from "../storeAndContext/UserProvider";
 
 export default function RoutesContainer(){
+
+    const {user} = useContext(UserContext)
     return(
         <div>
             <Routes>
                 <Route path="/" element={<Welcome/>}></Route>
                 <Route path="/home" element={<Home />}></Route>
                 {/* <Route path="/profile" children={()=><Profile></Profile>}></Route> */}
-                <Route path="/profile" element={<Profile/>}></Route>
+                {user ? <Route path="/profile" element={<Profile/>}></Route> : ''}
                 <Route path="/about" element={<About/>}></Route>
                 <Route path="/contact" element={<Contact/>}></Route>
                 <Route path="/login" element={<Auth></Auth>}></Route>
