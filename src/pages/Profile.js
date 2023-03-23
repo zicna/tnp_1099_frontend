0df6e6fc97a93cf22a5db7bfae14e86b684fb76c
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2023-03-22 19:23:31 -0500

    get user from context

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -3,9 +3,10 @@ import { useNavigate } from 'react-router-dom'
 import { UserContext } from '../storeAndContext/UserProvider'
 
 export default function Profile() {
+    const {user} = useContext(UserContext)
   return (
     <div>
-      <div>{user?.email}</div>
+      <div>{user.email}</div>
     </div>
   )
 }
