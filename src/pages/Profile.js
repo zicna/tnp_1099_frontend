5468a7bc77a5d523c10258dcc7a37662270b270a
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2023-03-22 19:21:05 -0500

    render user email

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useContext, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { UserContext } from '../storeAndContext/UserProvider'
 
-export default function Profile(){
-    return(
-        <div>
-            Profile Page
-        </div>
-        )
+export default function Profile() {
+  return (
+    <div>
+      <div>{user?.email}</div>
+    </div>
+  )
 }
