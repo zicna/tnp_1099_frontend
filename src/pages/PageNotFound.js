7a1438041e8ac0876acec899e4b89e0ccf537544
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2023-03-22 19:18:43 -0500

    every render of PageNotFound redirect to '/' after 3 seconds

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function PageNotFound(){
+    const navigate = useNavigate()
+
+    useEffect(()=>{
+        setTimeout(()=> navigate("/"),3000)
+    }, [])
     return (
         <h2>Page Not Found</h2>
     )
