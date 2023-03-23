89da2b050856880bfc8486d18cd4d1ed282d3502
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2023-03-22 19:10:50 -0500

    wrapp whole app with new component which will provide user through useCOntext to every component

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import './index.css'
 import App from './App'
 
 import { BrowserRouter as Router } from 'react-router-dom'
+import { UserProvider } from './storeAndContext/UserProvider'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <UserProvider>
+        <App />
+      </UserProvider>
     </Router>
   </React.StrictMode>
 )
