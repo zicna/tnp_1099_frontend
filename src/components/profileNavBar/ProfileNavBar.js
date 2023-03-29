f8b3c14a7bc65bb7b7cd7b6d10fc1ea3d7f204af
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2023-03-29 09:38:23 -0500

    add ProfileNavBar component for all user specific navigation

---
+++ src/components/profileNavBar/ProfileNavBar.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+export default function ProfileNavBar(){
+    return(
+        <div>Hello from Profile NAv Bar</div>
+    )
+}
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,11 +1,13 @@
 import React, { useContext, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { UserContext } from '../storeAndContext/UserProvider'
+import ProfileNavBar from '../components/profileNavBar/ProfileNavBar'
 
 export default function Profile() {
     const {user} = useContext(UserContext)
   return (
     <div>
+      <ProfileNavBar></ProfileNavBar>
       <div>{user.email}</div>
     </div>
   )
