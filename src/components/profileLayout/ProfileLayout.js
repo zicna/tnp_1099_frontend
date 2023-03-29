21f05616affabd33af845d1ae2026211a55c75d4
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2023-03-29 12:13:31 -0500

    change name and add '<Outlet>', with comments

---
+++ src/components/profileLayout/ProfileLayout.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import styles from './profileLayout.module.css'
+import { NavLink, Outlet } from 'react-router-dom'
+
+export default function ProfileLayout() {
+  return (
+    <div className={styles.container}>
+      <div>
+        <NavLink to="/profile">LOGO</NavLink>
+      </div>
+      <div>
+        <li>
+          <NavLink to="/profile/earnings">EARNINGS</NavLink>
+        </li>
+        <li>
+          <NavLink to="/profile/trips">TRIPS</NavLink>
+        </li>
+        <li>
+          <NavLink to="/profile/expenses">EXPENSES</NavLink>
+        </li>
+        <Outlet />
+      </div>
+    </div>
+  )
+}
--- src/components/profileNavBar/ProfileNavBar.js
+++
@@ -1,7 +0,0 @@
-import React from "react"
-
-export default function ProfileNavBar(){
-    return(
-        <div>Hello from Profile NAv Bar</div>
-    )
-}
