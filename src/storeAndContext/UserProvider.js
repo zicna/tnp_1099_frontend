ff252d581e7ae90223f18cdae97f332185e1570e
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2023-03-29 09:50:46 -0500

    add dummy data for the user for development purposes

--- src/storeAndContext/UserProvider.js
+++ src/storeAndContext/UserProvider.js
@@ -1,9 +1,16 @@
 import React, { createContext, useState } from 'react'
 
+// * dummy data 
+const obj = {
+  dateOfBirth: '2023-03-28T00:00:00.000+00:00',
+  email: 'ruby_master@example.com',
+  gender: 'FEMALE',
+  id: 3,
+}
 export const UserContext = createContext(null)
 
 export const UserProvider = ({ children }) => {
-  const [user, setUser] = useState(null)
+  const [user, setUser] = useState(obj)
 
   return (
     <UserContext.Provider value={{ user, setUser }}>
@@ -11,3 +18,4 @@ export const UserProvider = ({ children }) => {
     </UserContext.Provider>
   )
 }
+
